7f630a29ccbeaa61e2bd9529201c5f898d8660b6
NITA-PC\Santosh <nitakumari94@gmail.com>
2018-03-25 21:13:21 +0530

    temp-changed Nita

--- HelloWorld/src/com/nita/HelloWorld.java
+++ HelloWorld/src/com/nita/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello  nita from Different project!!");
+		System.out.println("Hello  Nita from Different project!!");
 		System.out.println("Hello java world !!");
 	}
 
