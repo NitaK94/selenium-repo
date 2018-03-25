d37cbd0534504b89ade74bc059b74a88d6bd42a6
NITA-PC\Santosh <nitakumari94@gmail.com>
2018-03-25 21:04:07 +0530

    temp- added com.nita package

--- HelloWorld/src/com/nita/HelloWorld.java
+++ HelloWorld/src/com/nita/HelloWorld.java
@@ -1,9 +1,9 @@
-
+package com.nita;
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello  nita !!");
+		System.out.println("Hello  nita from Different project!!");
 		System.out.println("Hello java world !!");
 	}
 
