ec6609143262e408b6db701137d85ceafb2d70eb
NITA-PC\Santosh <nitakumari94@gmail.com>
2018-04-17 21:00:54 +0530

    rippling4

--- ripplingproject/src/main/java/com/genricLib/BaseClass.java
+++ ripplingproject/src/main/java/com/genricLib/BaseClass.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -13,37 +14,48 @@ import org.testng.annotations.BeforeMethod;
 
 public class BaseClass {
 	public  WebDriver driver;
+	
 	  @BeforeClass
-	   public void configBC() throws Throwable{
-		  CommonUtil g = new CommonUtil();
-		  Properties p = g.getdropdataObj();
-		  String pBrowser = p.getProperty("browser");
-		  if(pBrowser.equals("FireFox")){
-			  driver = new FirefoxDriver();
-		  }
-		  else if(pBrowser.equals("chrome")){
-			  System.setProperty("webdriver.chrome.driver","./srcmain\\resources\\chromedriver.exe");
+	    public void configBC() throws Throwable
+	      {
+		    CommonUtil g = new CommonUtil();
+		    /* take browser from properties file*/
+		     Properties p = g.getdropdataObj();
+		     String pBrowser = p.getProperty("browser");
+		     
+		  if(pBrowser.equals("FireFox"))
+		      {
+			    driver = new FirefoxDriver();
+		      }
+		  
+		  else if(pBrowser.equals("chrome"))
+		     {
+			    System.setProperty("webdriver.chrome.driver","./srcmain\\resources\\chromedriver.exe");
 				driver = new ChromeDriver();
-		  }
-		  else if(pBrowser.equals("ie")){
+		     }
+		  
+		  else if(pBrowser.equals("ie"))
+		   {
 			  System.setProperty("webDriver.IE.driver",".\\src\\main\\resources\\IEDriverServer.exe");
-			
-		  }
-		  else{
+			}
+		  
+		  else
+		    {
 			  driver = new FirefoxDriver();
-		  }
-		 
+		    }
+		  Reporter.log("Browser lounch",true);
 		 
 		}
 			
 	@BeforeMethod
 	   public void  configBM() throws Throwable{
-		
-		CommonUtil g = new CommonUtil();
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-		Properties p = g.getdropdataObj();
-		String url = p.getProperty("urldata");
-			 driver.get(url);
+		/* take url from properties file*/
+		CommonUtil g = new CommonUtil();
+		 Properties p = g.getdropdataObj();
+		 String url = p.getProperty("urldata");
+		 Reporter.log("navigate to Link",true);
+		   driver.get(url);
 			 
 		}
 	
