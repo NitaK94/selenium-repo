710c77218aa3c9f9282fbb0916c1f5f9f5847b6b
NITA-PC\Santosh <nitakumari94@gmail.com>
2018-04-17 20:40:07 +0530

    rippling first

---
+++ ripplingproject/src/main/java/com/crm/genricLib/CommonUtil.java
@@ -0,0 +1,40 @@
+package com.crm.genricLib;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class CommonUtil {
+	public String getData(String Sheetnum,int RowNum,int ColNum) throws EncryptedDocumentException, InvalidFormatException, IOException {
+		String path = "./src/test/resources/data/TestData.xlsx";
+		//get the location of the xmls file
+				
+		FileInputStream fls = new FileInputStream(path);
+				//open workbook in read
+				Workbook wb = WorkbookFactory.create(fls);
+				//get the control of sheet2
+				Sheet sh = wb.getSheet(Sheetnum);
+				//get the control in sheet 3 in row
+				Row row = sh.getRow(RowNum);
+				//read date from 2,3,4 col
+				String data = row.getCell(ColNum).getStringCellValue();
+				return data;
+	}
+		
+	 public Properties getdropdataObj() throws IOException{
+		String path = "./src/test/resources/data/testData.properties";
+		FileInputStream fls1 = new FileInputStream(path);
+		Properties prop = new Properties();
+		prop.load(fls1);
+		
+		return prop;
+		
+	}	
+}
---
+++ ripplingproject/src/test/java/rippling/VerifyPhoneOrNumber.java
@@ -0,0 +1,25 @@
+package rippling;
+
+import org.testng.annotations.Test;
+
+public class VerifyPhoneOrNumber {
+	@Test
+	public void verifyPhoneNumberWithValidData(){
+		
+	}
+	@Test
+    public void verifyPhoneNumberWithInValidData(){
+		
+	}
+	@Test
+    public void verifyNewNumberWithInValidData(){
+		
+	}
+	
+	@Test
+    public void verifyNewNumberWithValidData(){
+		
+	}
+
+
+}
