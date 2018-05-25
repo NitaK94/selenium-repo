44ae1285d69a3b34a44287a985c05304dd368f51
NITA-PC\Santosh <nitakumari94@gmail.com>
2018-05-25 11:03:59 +0530

    s56rdtf6ygi7uhlk

--- SeleniumActtimeFrameWork/src/com/actitime/objectropositorLib/Login.java
+++ SeleniumActtimeFrameWork/src/com/actitime/objectropositorLib/Login.java
@@ -4,30 +4,32 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class Login {
-  @FindBy(name="username") WebElement UserEdt;
-  @FindBy(name="pwd") WebElement PwdEdt;
+  
+@FindBy(name="username") WebElement UserEdt;
+ 
+@FindBy(name="pwd") WebElement PwdEdt;
   @FindBy(id="loginButton") WebElement LoginEdt;
  
-public WebElement getUserEdt() {
-	return UserEdt;
-}
-
-public WebElement getPwdEdt() {
-	return PwdEdt;
-}
-
-public WebElement getLoginEdt() {
-	return LoginEdt;
-}
+  
+  public WebElement getUserEdt() {
+		return UserEdt;
+	}
+	public WebElement getPwdEdt() {
+		return PwdEdt;
+	}
+	public WebElement getLoginEdt() {
+		return LoginEdt;
+	}
+  
+  
 
 public void login(String username, String pwd){
 	  UserEdt.sendKeys(username);
 	  PwdEdt.sendKeys(pwd);
 	  LoginEdt.click();
 }
-
 public void login(){
-	 LoginEdt.click(); 
+	  LoginEdt.click();
 }
 
 }
--- SeleniumActtimeFrameWork/src/com/actitime/testscript/CreateCustomer.java
+++ SeleniumActtimeFrameWork/src/com/actitime/testscript/CreateCustomer.java
@@ -1,16 +1,20 @@
 package com.actitime.testscript;
 
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.actitime.genericLib.BaseClass;
+import com.actitime.objectropositorLib.Login;
 @Listeners(com.actitime.genericLib.SamplerListner.class)
 public class CreateCustomer extends BaseClass{
    @Test
    public void CreatTest(){
 	   System.out.println("passs=====================");
 	   Assert.assertEquals("fgh", "dfg");
+	   Login lg = PageFactory.initElements(driver, Login.class);
+	  lg.getLoginEdt().click();
    }
 	
 
